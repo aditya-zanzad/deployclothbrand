abec8e6ba2fa8da5b5a4df9ce15a746bc7b90079
Aditya Zanzad <115469452+aditya-zanzad@users.noreply.github.com>
2025-06-28 02:20:04 +0530

    Update userRoutes.js
    nwe

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -360,9 +360,9 @@ router.put("/:id/verify", async (req, res) => {
   }
 });
 
-const FRONTEND_URL = process.env.FRONTEND_URL;
-const EMAIL_USER = process.env.EMAIL_USER;
-const EMAIL_PASS = process.env.EMAIL_PASS;
+FRONTEND_URL=https://www.wholesalebaba.online
+EMAIL_USER=wholesalebabaa@gmail.com
+EMAIL_PASS=sfuz vzgw zogf rqpn
 
 // Forgot Password Route
 router.post("/api/auth/forgot-password", async (req, res) => {
